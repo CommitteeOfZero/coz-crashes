ad1e86b083534b09e31b256e8586819ec01d8746
Niklas K <drdaxxy@googlemail.com>
2018-08-22 12:41:20 +0200

    more public/admin route separation

--- src/routes.php
+++ src/routes.php
@@ -5,11 +5,12 @@ $app->post('/submit', Actions\SubmitAction::class);
 
 // Static index page for public report retrieval/deletion
 $app->get('/gdpr', Actions\GdprIndexAction::class);
+$app->redirect('/', '/gdpr', 308);
 
 //
 // admin area
 //
 
-$app->get('/view/{id}', function ($request, $response, $args) {
+$app->get('/admin/view/{id}', function ($request, $response, $args) {
     // dummy
 })->setName('adminView');
